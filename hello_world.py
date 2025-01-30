3b9755312310b5fed6c4c5d2fede4e195c491019
Daniel Schäftner <daniel.schaeftner@schaeftner-engineering.com>
2025-01-30 17:07:51 +0100

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,46 @@
+# # city = "Ich bin in FN!!!!!"
+# # company = "Microsoft"
+# # job_title = "Engineer"
+# name = "Daniel"
+# last_name = "Schäftner" 
+# full_name = name + last_name
+
+# print("My name is: \"in Anführungszeichen\" ")
+
+# active_users = 0.5
+# stundents_in_class = 45
+# print(stundents_in_class / active_users)
+
+# powered_on = False
+
+# print(not powered_on)
+
+# entered_pin = 5444
+# expected_pin = 5555
+# result = entered_pin != expected_pin
+
+
+# number_1 = 14
+# number_2 = 13
+
+# result = number_1 >= number_2
+
+# print(result)
+item_1 = "meckatzer"
+item_2 = "becks"
+
+# is_dublicated = item_1 == item_2
+
+# text = "string"
+anzahl = 5
+kosten = 1.6
+# boolsche = True
+
+# formated print
+# print(f"{anzahl} meckatzer für {kosten}€ bitte")
+
+# print( str(anzahl) + " messgage")
+
+val = input("Enter a value: ")
+print(val)
+print("git")
---
+++ kilometer_converter.py
@@ -0,0 +1,19 @@
+'''Wir wollen Meilen in Kilometer umrechnen und ausgeben.
+Bitte schreibe das Programm'''
+
+# Author: Daniel S.
+# Date: 16.01.2025
+
+### CONSTANTS
+FACTOR = 1.609344 # this is the factor miles to km
+
+### PROGRAMM
+
+# Inputs
+miles = float(input("Please enter miles to be calculated: "))
+
+# Process
+km = miles * FACTOR  
+
+# Output
+print("This is your result in km: " + str(km))
