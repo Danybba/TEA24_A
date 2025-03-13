f3094b249025a7f11262bca4135d7b4d941b905e
Daniel Schäftner <daniel.schaeftner@schaeftner-engineering.com>
2025-03-13 17:23:08 +0100

    added module examples

--- hello_world.py
+++ hello_world.py
@@ -1,76 +1,238 @@
-# # city = "Ich bin in FN!!!!!"
-# # company = "Microsoft"
-# # job_title = "Engineer"
-# name = "Daniel"
-# last_name = "Schäftner" 
-# full_name = name + last_name
+# import tkinter as tk
+# import turtle
 
-# print("My name is: \"in Anführungszeichen\" ")
+# # # city = "Ich bin in FN!!!!!"
+# # # company = "Microsoft"
+# # # job_title = "Engineer"
+# # name = "Daniel"
+# # last_name = "Schäftner" 
+# # full_name = name + last_name
 
-# active_users = 0.5
-# stundents_in_class = 45
-# print(stundents_in_class / active_users)
+# # print("My name is: \"in Anführungszeichen\" ")
 
-# powered_on = False
+# # active_users = 0.5
+# # stundents_in_class = 45
+# # print(stundents_in_class / active_users)
 
-# print(not powered_on)
+# # powered_on = False
 
-# entered_pin = 5444
-# expected_pin = 5555
-# result = entered_pin != expected_pin
+# # print(not powered_on)
+
+# # entered_pin = 5444
+# # expected_pin = 5555
+# # result = entered_pin != expected_pin
 
 
-# number_1 = 14
-# number_2 = 13
+# # number_1 = 14
+# # number_2 = 13
 
-# result = number_1 >= number_2
+# # result = number_1 >= number_2
 
-# print(result)
-item_1 = "meckatzer"
-item_2 = "augustiner"
+# # # print(result)
+# # item_1 = "meckatzer"
+# # item_2 = "augustiner"
 
-# is_dublicated = item_1 == item_2
+# # is_dublicated = item_1 == item_2
 
-# text = "string"
-anzahl = 5
-kosten = 1.6778
-# boolsche = True
+# # text = "string"
+# # anzahl = 5
+# # kosten = 1.6778
+# # boolsche = True
 
-# formated print
-# print(f"{anzahl} meckatzer für {kosten}€ bitte")
+# # formated print
+# # print(f"{anzahl} meckatzer für {kosten}€ bitte")
 
-# print( str(anzahl) + " messgage")
+# # print( str(anzahl) + " messgage")
 
-# val = input("Enter a value: ")
-# print(val)
-# print("git")
+# # val = input("Enter a value: ")
+# # print(val)
+# # print("git")
 
-# condition = False
+# # condition = False
 
-# if condition:
-#     print("condition is true")
+# # if condition:
+# #     print("condition is true")
 
-# age = int(input("Enter your age: "))
+# # age = int(input("Enter your age: "))
 
-# if (age >= 18):
-#     print("You are an adult")
+# # if (age >= 18):
+# #     print("You are an adult")
 
-# hour = int(input("Enter the hour: "))
+# # hour = int(input("Enter the hour: "))
 
-# if hour < 12:
-#     print("Good morning")
-# elif hour < 18:
-#     print("Good afternoon")
-# elif hour < 22:
-#     print("Good evening")
-# else:
-#     print("Good night")
+# # if hour < 12:
+# #     print("Good morning")
+# # elif hour < 18:
+# #     print("Good afternoon")
+# # elif hour < 22:
+# #     print("Good evening")
+# # else:
+# #     print("Good night")
 
-age = int(input("Enter your age: "))
-has_permission = True
-has_license = True	
-alcohol_level = 0.5
-has_insurance = True
+# # age = int(input("Enter your age: "))
+# # has_permission = True
+# # has_license = True	
+# # alcohol_level = 0.5
+# # has_insurance = True
 
-if (18 <= age) and ((True == has_permission) or (has_license == True)) and (alcohol_level <= 0.3) and has_insurance:
-    print("You are allowed to drive")
+# # if (18 <= age) and ((True == has_permission) or (has_license == True)) and (alcohol_level <= 0.3) and has_insurance:
+# #     print("You are allowed to drive")
+
+
+# # wallet = 3
+# # wallet = wallet + 1
+# # wallet = wallet - 2
+# # print(wallet)
+
+# # name = "Daniel"
+# # name = name + " Schäftner"
+# # print(name)
+
+# # sales = 5
+# # sales += 2
+# # sales -= 1
+# # sales *= 2
+# # sales /= 2  
+# # print(sales)
+
+# # counter = 3
+
+# # while counter < 8:
+# #     print(counter)
+# #     counter += 2
+
+
+# # for i in range(7, 2, -2):
+# #     print(i, end=" ")
+
+
+# # lines = int(input("Enter the number of lines: "))
+
+# # for i in range(1, lines + 1):
+# #     print('*' * i)
+
+# # for i in range(lines - 1, 0, -1):
+# #     print('*' * i)
+
+
+# # a, b = 0, 1
+# # while b <= 50:
+# #     print(b)
+# #     a, b = b, a + b
+
+
+# # def greet_user(name):
+# #     print(f"Hello {name}")
+
+
+# # greet_user("Anna")
+# # greet_user("Tom")
+
+# # def display_half(number):
+# #     print(number / 2)
+
+# # def display_double(number):
+# #     print(number * 2)
+
+# # def add_two_numbers(number_1, number_2):
+# #     return number_1 + number_2
+
+
+# # display_half(10)
+
+# # display_double(10)
+
+# # result = add_two_numbers(10, 20)
+
+# # print(result)
+
+
+
+# # def add_bonus(salary):
+# #     global start_value
+# #     start_value = 200
+# #     bonus = 100
+# #     print(salary + bonus + start_value)
+
+
+# # add_bonus(1000)
+
+# # print(start_value)
+
+# # age = [20, 30, 40, 50, 60]
+
+# # age.append(70)
+# # age.insert(2, 25)
+# # age.pop(3)
+
+# # print(age)
+# # print(len(age))
+
+# # for ages in age:
+# #     print(ages)
+
+
+# # print(dir["name"])
+
+# # todo = ["clean the house", "buy groceries"]
+# # print(todo)
+
+# # import math as m
+
+# # from math import pi
+
+# # print(1+m.floor(2.44))
+
+# # from arrow_function_lib import arrow
+
+# # size = int(input("Enter the size of the arrow: "))
+
+# # arrow(size)
+
+# # pie = pygal.Pie()
+
+# # pie.title = "Time spent on social networks"
+# # pie.add("Facebook", 30)
+# # pie.add("Instagram", 20)
+# # pie.add("Twitter", 15)
+
+# # pie.render_in_browser()
+
+# # def on_button_click():
+# #     label.config(text="Button clicked!")
+
+# # root = tk.Tk()
+# # root.title("Tkinter Example")
+
+# # label = tk.Label(root, text="Hello, Tkinter!")
+# # label.pack()
+
+# # button = tk.Button(root, text="Click Me", command=on_button_click)
+# # button.pack()
+
+# # root.mainloop()
+
+
+# # Set up the screen
+# wn = turtle.Screen()
+# wn.title("Turtle Example")
+# wn.bgcolor("white")
+
+# # Create a turtle named "t"
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.color("blue")
+
+# # Move the turtle forward
+# t.forward(100)
+
+# # Turn the turtle to the right
+# t.right(90)
+
+# # Move the turtle forward again
+# t.forward(100)
+
+# # Close the turtle graphics window when clicked
+# wn.exitonclick()
+
+
